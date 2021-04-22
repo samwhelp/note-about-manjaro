7232e6cbe16c0f1eef2cdead869a96421558bf76
samwhelp <samwhelp@users.noreply.github.com>
2021-04-22 23:01:00 +0800

    nvimrc-ctrl

---
+++ _demo/project/vimrc-profile/nvimrc-ctrl/gen/nvimrc-ctrl/Section/Model/install/Core.sh
@@ -0,0 +1,27 @@
+
+
+################################################################################
+### Head: Model vimrc_profile_install
+##
+
+vimrc_profile_install () {
+
+	## $ nvimrc-ctrl install demo https://github.com/samwhelp/tool-svim.git
+
+	local name="$1"
+	local repo_url="$2"
+
+	echo "cd $THE_VIMRC_PROFILE_DIR_PATH"
+	cd "$THE_VIMRC_PROFILE_DIR_PATH" ## cd ~/.vimrc-profile
+
+
+	echo "git clone $repo_url $name"
+	git clone "$repo_url" "$name" ## git clone https://github.com/samwhelp/tool-svim.git demo
+
+	cd "$OLDPWD"
+
+}
+
+##
+### Tail: Model vimrc_profile_install
+################################################################################
