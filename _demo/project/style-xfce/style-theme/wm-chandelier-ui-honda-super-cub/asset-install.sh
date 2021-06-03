b866977dfa690311b69dced3591e79f308382064
samwhelp <samwhelp@users.noreply.github.com>
2021-06-03 22:00:59 +0800

    wm-chandelier-ui-honda-super-cub

--- _demo/project/style-xfce/style-theme/wm-chandelier-ui-honda-super-cub/asset-install.sh
+++ _demo/project/style-xfce/style-theme/wm-chandelier-ui-honda-super-cub/asset-install.sh
@@ -8,60 +8,15 @@
 ##
 theme_asset_install () {
 
-	theme_asset_install_theme_honda_super_cub
-	theme_asset_install_theme_chandelier
+	theme_asset_install_wm_chandelier
+	theme_asset_install_ui_honda_super_cub
 
 }
 
-theme_asset_install_theme_honda_super_cub () {
 
-	theme_asset_install_theme_honda_super_cub_clone
+theme_asset_install_wm_chandelier () {
 
-
-	#echo "$(pacman -Ql xfce4-settings | grep appearance-install-theme | cut -d ' ' -f 2) 'asset/reorr_gtk-my-theme-collection/Honda Super Cub'"
-	#/usr/lib/xfce4/settings/appearance-install-theme 'asset/reorr_gtk-my-theme-collection/Honda Super Cub'
-	#$(pacman -Ql xfce4-settings | grep appearance-install-theme | cut -d ' ' -f 2) 'asset/reorr_gtk-my-theme-collection/Honda Super Cub'
-
-	theme_asset_install_theme_honda_super_cub_to_local
-
-	echo
-
-	file "$HOME/.themes/Honda-Super-Cub"
-	ls -l "$HOME/.themes/Honda-Super-Cub"
-
-	echo
-
-}
-
-theme_asset_install_theme_honda_super_cub_clone () {
-
-	if [ -a 'asset/reorr_gtk-my-theme-collection' ]; then ## file is exists
-		echo "File Exists: asset/reorr_gtk-my-theme-collection"
-		return 0
-	fi
-
-	## https://github.com/reorr/my-theme-collection/tree/master/Honda%20Super%20Cub
-
-	echo "git clone 'https://github.com/reorr/my-theme-collection.git' 'asset/reorr_gtk-my-theme-collection'"
-	git clone 'https://github.com/reorr/my-theme-collection.git' 'asset/reorr_gtk-my-theme-collection'
-
-}
-
-theme_asset_install_theme_honda_super_cub_to_local () {
-	if [ -a "$HOME/.themes/Honda-Super-Cub" ]; then ## file is exists
-		echo "File Exists: $HOME/.themes/Honda-Super-Cub"
-		return 0
-	fi
-
-	mkdir -p "$HOME/.themes"
-
-	cp -a 'asset/reorr_gtk-my-theme-collection/Honda Super Cub/.themes/Honda-Super-Cub' "$HOME/.themes/Honda-Super-Cub"
-	##cp -a 'asset/reorr_gtk-my-theme-collection/Honda Super Cub/.themes/Honda-Super-Cub' "$HOME/.local/share/themes/Honda-Super-Cub"
-}
-
-theme_asset_install_theme_chandelier () {
-
-	theme_asset_install_theme_chandelier_clone
+	theme_asset_install_wm_chandelier_clone
 
 	echo "$(pacman -Ql xfce4-settings | grep appearance-install-theme | cut -d ' ' -f 2) 'asset/addy-dclxvi_xfwm4-theme-collections/Chandelier'"
 	#/usr/lib/xfce4/settings/appearance-install-theme 'asset/Drzaln_Nema/Chandelier'
@@ -77,7 +32,7 @@ theme_asset_install_theme_chandelier () {
 
 }
 
-theme_asset_install_theme_chandelier_clone () {
+theme_asset_install_wm_chandelier_clone () {
 	if [ -a 'asset/addy-dclxvi_xfwm4-theme-collections' ]; then ## file is exists
 		echo "File Exists: asset/addy-dclxvi_xfwm4-theme-collections"
 		return 0
@@ -91,6 +46,55 @@ theme_asset_install_theme_chandelier_clone () {
 		echo "git clone 'https://github.com/addy-dclxvi/xfwm4-theme-collections.git' 'asset/addy-dclxvi_xfwm4-theme-collections'"
 
 }
+
+
+theme_asset_install_ui_honda_super_cub () {
+
+	theme_asset_install_ui_honda_super_cub_clone
+
+
+	#echo "$(pacman -Ql xfce4-settings | grep appearance-install-theme | cut -d ' ' -f 2) 'asset/reorr_gtk-my-theme-collection/Honda Super Cub'"
+	#/usr/lib/xfce4/settings/appearance-install-theme 'asset/reorr_gtk-my-theme-collection/Honda Super Cub'
+	#$(pacman -Ql xfce4-settings | grep appearance-install-theme | cut -d ' ' -f 2) 'asset/reorr_gtk-my-theme-collection/Honda Super Cub'
+
+	theme_asset_install_ui_honda_super_cub_to_local
+
+	echo
+
+	file "$HOME/.themes/Honda-Super-Cub"
+	ls -l "$HOME/.themes/Honda-Super-Cub"
+
+	echo
+
+}
+
+theme_asset_install_ui_honda_super_cub_clone () {
+
+	if [ -a 'asset/reorr_gtk-my-theme-collection' ]; then ## file is exists
+		echo "File Exists: asset/reorr_gtk-my-theme-collection"
+		return 0
+	fi
+
+	## https://github.com/reorr/my-theme-collection/tree/master/Honda%20Super%20Cub
+
+	echo "git clone 'https://github.com/reorr/my-theme-collection.git' 'asset/reorr_gtk-my-theme-collection'"
+	git clone 'https://github.com/reorr/my-theme-collection.git' 'asset/reorr_gtk-my-theme-collection'
+
+}
+
+theme_asset_install_ui_honda_super_cub_to_local () {
+	if [ -a "$HOME/.themes/Honda-Super-Cub" ]; then ## file is exists
+		echo "File Exists: $HOME/.themes/Honda-Super-Cub"
+		return 0
+	fi
+
+	mkdir -p "$HOME/.themes"
+
+	cp -a 'asset/reorr_gtk-my-theme-collection/Honda Super Cub/.themes/Honda-Super-Cub' "$HOME/.themes/Honda-Super-Cub"
+	##cp -a 'asset/reorr_gtk-my-theme-collection/Honda Super Cub/.themes/Honda-Super-Cub' "$HOME/.local/share/themes/Honda-Super-Cub"
+}
+
+
 
 
 ##
